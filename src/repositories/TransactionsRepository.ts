e7cfe07efb4cb884913de4462b61b3aaae0fe417
Claudio <claudio.jr@targetone.com.br>
2020-11-08 15:21:01 -0300

    Criadas funcionalidades necessárias

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,22 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.filter(transaction => transaction.type === 'income').map(transaction => transaction.value).reduce((a, b) => a + b, 0);
+    const totalOutcome = this.transactions.filter(transaction => transaction.type === 'outcome').map(transaction => transaction.value).reduce((a, b) => a + b, 0);
+
+    return { income: totalIncome, outcome: totalOutcome, total: totalIncome - totalOutcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: 'income' | 'outcome'): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
